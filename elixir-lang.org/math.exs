36a9c2dd2d32977c9afa5e4c3dc7654a67fd0c12
Tad Thorley <phaedryx@gmail.com>
2017-03-18 21:00:36 -0600

    function pattern matching arguments

--- elixir-lang.org/math.exs
+++ elixir-lang.org/math.exs
@@ -6,7 +6,17 @@ defmodule Math do
   def product(a, b) do
     a * b
   end
+
+  def zero?(0) do
+    true
+  end
+
+  def zero?(x) when is_integer(x) do
+    false
+  end
 end
 
 IO.puts Math.sum(1,1)
 IO.puts Math.product(2,3)
+IO.puts Math.zero?(0)
+Io.puts Math.zero?(1)
